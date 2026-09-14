4321b955798854cdc6a6abedc590eefbfc12e551
SIP Developer <sip@sic.gov.ao>
2026-09-14 23:30:18 +0100

    fix: remove broken SipProcess spec, fix audit event factory
    - Removed spec/models/sip_process_spec.rb (references removed model)
    - Fixed process_audit_event factory to set created_at
    - Model specs now pass
    
    Task: TASK-AUDIT follow-up

--- spec/factories/process_audit_events.rb
+++ spec/factories/process_audit_events.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :process_audit_event do
     action { "PROCESSO_CRIADO" }
     entity_type { "Process" }
+    created_at { Time.current }
     
     process { create(:process) }
     actor { create(:user) }
--- spec/models/sip_process_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe SipProcess, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
